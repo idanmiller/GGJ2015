23ef69a9a5a8d94b3a6c6c58aecdf11c938a5996
Erez Bursztyn <erez@slatescience.com>
2015-01-22 01:36:27 +0200

    Change Entity to CircularEntity

--- code/Bacteria.js
+++ code/Bacteria.js
@@ -1,5 +1,5 @@
 Bacteria = function(game, x, y, resource, collisionRadius) {
-    Entity.call(this, game, x, y, resource, collisionRadius);
+    CircularEntity.call(this, game, x, y, resource, collisionRadius);
 };
 
 Bacteria.prototype = Object.create(Entity.prototype);
---
+++ code/CircularEntity.js
@@ -0,0 +1,17 @@
+CircularEntity = function(game, x, y, resource, collisionRadius) {
+    Phaser.Sprite.call(this, game, x, y, resource);
+    this.collisionRadius = collisionRadius;
+};
+
+CircularEntity.prototype = Object.create(Phaser.Sprite.prototype);
+CircularEntity.prototype.constructor = CircularEntity;
+
+CircularEntity.prototype = {
+
+	getDistance: function(otherCircularEntity) {
+		var dx = otherCircularEntity.x - this.x;
+        var dy = otherEntity.y - this.y;
+        return Math.sqrt(dx * dx + dy * dy);
+	}
+
+}; 
