9dc676b0f06e63b04691cde8a6b63911261b9050
Erez Bursztyn <erez@slatescience.com>
2015-01-22 01:17:25 +0200

    Initial commit of Entity and Bacteria classes

---
+++ code/Bacteria.js
@@ -0,0 +1,12 @@
+Bacteria = function(game, x, y, resource, collisionRadius) {
+    Entity.call(this, game, game.x, game.y, resource, collisionRadius);
+};
+
+Bacteria.prototype = Object.create(Entity.prototype);
+Bacteria.prototype.constructor = Bacteria;
+
+Bacteria.prototype = {
+
+
+
+}; 
---
+++ code/Entity.js
@@ -0,0 +1,13 @@
+BasicGame.Entity = function() {
+
+};
+
+BasicGame.Entity.prototype = {
+
+	init: function(x, y) {
+		this.x = x;
+		this.y = y;
+	},
+
+
+} 
