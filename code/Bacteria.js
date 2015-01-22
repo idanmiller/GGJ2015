54fd1ad0f3075f6e34f28c2d6d203e276377b30c
Erez Bursztyn <erez@slatescience.com>
2015-01-22 02:38:09 +0200

    Split Bacteria

--- code/Bacteria.js
+++ code/Bacteria.js
@@ -4,3 +4,11 @@ Bacteria = function(game, x, y, resource, collisionRadius) {
 
 Bacteria.prototype = Object.create(CircularEntity.prototype);
 Bacteria.prototype.constructor = Bacteria;
+
+Bacteria.prototype.split = function() {
+	var first = new Bacteria(this.game, x, y-10, resource, collisionRadius);
+	var second = new Bacteria(this.game, x, y-10, resource, collisionRadius);
+	first.scale.setTo(0.8, 0.8);
+	second.scale.setTo(0.8, 0.8);
+	return [first, second];
+}; 
